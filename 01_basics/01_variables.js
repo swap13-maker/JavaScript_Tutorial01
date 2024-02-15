faabcf9bbc7506161b76f094d585bcc9a6dda987
Swapnil Lule <82203579+swap13-maker@users.noreply.github.com>
2024-02-15 15:00:10 +0000

    variables changes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -11,3 +11,17 @@ accountCity="Jaipur";
 
 console.table([accountId, accountEmail, accountState, accountCity]);
 
+//difference between let, var, const
+
+/*
+let and const are the local variables 
+var is global variable 
+
+let and const cannot achieve the functionality of hosting
+by  using var we can achieve the functionality of hosting
+by using hosting we can call the method before it is created
+
+
+*/
+
+
