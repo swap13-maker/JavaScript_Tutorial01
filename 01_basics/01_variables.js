bce62d8f8a128b6760852e842109f69049b015a9
Swapnil Lule <82203579+swap13-maker@users.noreply.github.com>
2024-02-15 11:41:54 +0000

    variables config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=45566
+let accountEmail="swapnillu13@gmail.com";
+let accountState="Maharashtra";
+let accountCity="Pune";
+
+accountEmail="swapnil7989@gmail.com";
+accountState="Rajastan";
+accountCity="Jaipur";
+
+
+
+console.table([accountId, accountEmail, accountState, accountCity]);
+
