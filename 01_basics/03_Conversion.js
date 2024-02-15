ea719c15f645f7c2b38589d83685c5688209b267
Swapnil Lule <82203579+swap13-maker@users.noreply.github.com>
2024-02-15 14:59:11 +0000

    Conversion of datatypes

---
+++ 01_basics/03_Conversion.js
@@ -0,0 +1,11 @@
+
+let someNumber= "33abc";
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+let isLoggenIn = "hitesh";
+let booleanValue= Boolean(isLoggenIn);
+console.log(booleanValue);
+console.log(typeof booleanValue);
+
