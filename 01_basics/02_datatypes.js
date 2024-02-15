91ea8fa4a1af2db1b0911da4e416b8b84ac44154
Swapnil Lule <82203579+swap13-maker@users.noreply.github.com>
2024-02-15 11:49:18 +0000

    datatype config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+let a=10; //it is type of number
+let b="swapnil"; //it is type of string
+
+console.log(b);
+console.log(a);
+
+//There are two types datatypes
+// 1) Primitive datatype
+// 2) Non-Primitive datatype
+
+/* 
+
+Premitive datatype
+String - represent sequence of character values.
+Number - represent numerice values
+boolean - represent value either true or false
+undefined - represent undefined values return type is undefined
+null- represent null values return type is object
+
+Non-Premitive datatype
+Array - represent group of similar values
+object
+regExp
+
+*/
+
