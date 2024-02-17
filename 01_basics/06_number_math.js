b6cc087d45410771e0c4a92be71aebe7423dace3
Swapnil Lule <82203579+swap13-maker@users.noreply.github.com>
2024-02-17 10:26:21 +0000

    container config done

---
+++ 01_basics/06_number_math.js
@@ -0,0 +1,10 @@
+
+const newString = "swapnil";
+console.log(newString.toUpperCase());
+console.log(newString.length);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
---
+++ 01_basics/07_dates.js
@@ -0,0 +1,17 @@
+
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toJSON());
+// console.log(typeof myDate);
+
+let myNewDate = new Date(2023, 0, 23);
+console.log(myNewDate.toDateString());
+
+let myNewDate1 = new Date(2023, 0, 23, 5, 3);
+console.log(myNewDate1.toLocaleString());
+
+
