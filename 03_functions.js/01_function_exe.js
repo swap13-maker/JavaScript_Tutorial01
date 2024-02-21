8165edb003f56160a4deb1d49dd380056ce8f62e
Swapnil Lule <82203579+swap13-maker@users.noreply.github.com>
2024-02-21 11:58:35 +0000

    function config added

--- 03_functions.js/01_function_exe.js
+++ 03_functions.js/01_function_exe.js
@@ -1,5 +1,5 @@
 //functions 
-
+//addition of two numbers
 function addTwoNumber(num1, num2){
 
     let sum = num1 + num2;
@@ -9,6 +9,7 @@ function addTwoNumber(num1, num2){
 let sum = addTwoNumber(18,20);
 console.log(sum);
 
+//login user function
 function loginUserMessage(username){
 
     let loginInfo = `${username} is logged in`;
@@ -19,3 +20,51 @@ function loginUserMessage(username){
 let loginInfoDetails = loginUserMessage("Swapnil Lule");
 console.log(loginInfoDetails);
 
+//calculate cart  price
+
+function calculateCartPrice(...num1){ //...means here we used rest operator 
+    return num1;
+
+}
+
+console.log(calculateCartPrice(200,400,500));
+
+//function calling using object
+
+const userDetails ={
+    username: "swapnil",
+    gender: 25,
+    email: "swapnillule13@gmail.com"
+}
+
+function callUserDetails(us){
+    return `my name is ${us.username} my gender is ${us.gender} my email ${us.email}`;
+}
+
+console.log(callUserDetails({
+    username: "NIkhil",
+    gender: 29,
+    email: "nikhil78@gmail.com"
+}));
+
+//funnction calling an array
+const myNewArray = [200,500,400,600];
+
+function returnSecondValue(getarray){
+    return getarray[2];
+
+}
+
+console.log(returnSecondValue( [500,600,700,800,900]));
+
+
+//new array
+console.log("new Array");
+const myNewArray1 = [200,500,400,600];
+
+function returnSecondValue(getarray){
+    console.log(getarray[2]); 
+
+}
+
+returnSecondValue(myNewArray1);
