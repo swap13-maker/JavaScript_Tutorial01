ef67773db80843fc8353d22fa0c1824378a27f64
Swapnil Lule <82203579+swap13-maker@users.noreply.github.com>
2024-02-21 18:56:09 +0000

    local 02 config done

---
+++ 04_local_global/02_local_global.js
@@ -0,0 +1,24 @@
+
+//function 1
+function addOne(num){
+    return num + 1;
+    
+}
+// console.log(addOne(5));
+
+//function 2
+const addTwo = function(num){
+    return num + 2;
+} 
+// console.log(addTwo(9));
+
+console.log(x);
+var x =5;
+console.log(x);
+
+sayHello();
+function sayHello(){
+    console.log("hello");
+}
+
+sayHello();
