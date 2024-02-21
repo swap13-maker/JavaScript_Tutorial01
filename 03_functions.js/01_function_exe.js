e4ac9987443b9d7176e02dc816ae6bce8c6c7bb5
Swapnil Lule <82203579+swap13-maker@users.noreply.github.com>
2024-02-21 09:39:11 +0000

    function config done

---
+++ 03_functions.js/01_function_exe.js
@@ -0,0 +1,21 @@
+//functions 
+
+function addTwoNumber(num1, num2){
+
+    let sum = num1 + num2;
+    return sum;
+}
+
+let sum = addTwoNumber(18,20);
+console.log(sum);
+
+function loginUserMessage(username){
+
+    let loginInfo = `${username} is logged in`;
+    return loginInfo;
+
+}
+
+let loginInfoDetails = loginUserMessage("Swapnil Lule");
+console.log(loginInfoDetails);
+
