985b9c900dcedbd6cbaea5b66c1a9ecbb1af0bd4
Swapnil Lule <82203579+swap13-maker@users.noreply.github.com>
2024-02-22 13:05:25 +0000

    if else changes done

--- 05_control_flow/01_if_else.js
+++ 05_control_flow/01_if_else.js
@@ -1,6 +1,14 @@
 // if Statement - it checks wether condition is true or not,
 // if the first condition is true first block is executed either second block is executed
 
+/*
+Use if -  to specify a block of code to be executed, if a specified condition is true
+Use else - to specify a block of code to be executed, if the same condition is false
+Use else if - to specify a new condition to test, if the first condition is false
+Use switch - to specify many alternative blocks of code to be executed
+
+*/
+
 const balance = 2000;
 
 if (balance <750) {
