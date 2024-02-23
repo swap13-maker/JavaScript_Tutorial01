2b51768d71510225e574d31fde78762963f5e937
Swapnil Lule <82203579+swap13-maker@users.noreply.github.com>
2024-02-23 10:13:01 +0000

    for of config done

---
+++ 05_control_flow/04_for_of.js
@@ -0,0 +1,15 @@
+//for of loop
+
+let myarray = [1,2,3,4,5,6];
+
+for (const val of myarray) {
+    console.log(val);
+}
+
+let newString = "Swapnil Lule";
+
+for (const name of newString) {
+    
+    console.log(`my name is ${name}`);
+    
+}
